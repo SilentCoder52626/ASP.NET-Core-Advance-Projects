a939bc4ca707ca12b98563a438aa4761f89638d9
common khadka <commonkhadka@gmail.com>
2025-03-16 21:20:11 +0545

    Add related entity classes

---
+++ Common-GraphQL-dotnet/Common-GraphQL-dotnet/Entity/GameReview.cs
@@ -0,0 +1,13 @@
+﻿namespace Common_GraphQL_dotnet.Entity
+{
+    public sealed class GameReview
+    {
+        public Guid GameId { get; set; }
+        public Game? Game { get; set; }
+        public Guid ReviewerId { get; set; }
+        public Reviewer? Reviewer { get; set; }
+        public int Rating { get; set; }
+        public string Summary { get; set; } = string.Empty;
+        public DateTime ReviewedOn { get; set; }
+    }
+}
---
+++ Common-GraphQL-dotnet/Common-GraphQL-dotnet/Entity/Reviewer.cs
@@ -0,0 +1,12 @@
+﻿namespace Common_GraphQL_dotnet.Entity
+{
+    public class Reviewer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Picture { get; set; } = string.Empty;
+        public ICollection<GameReview> GameReviews { get; set; } = new HashSet<GameReview>();
+    }
+}
