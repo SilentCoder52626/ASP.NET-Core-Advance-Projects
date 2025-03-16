a91160c784c986a2e33ace603db916c476d8b4ee
common khadka <commonkhadka@gmail.com>
2025-03-16 22:08:14 +0545

    Add reviewer query

--- Common-GraphQL-dotnet/Common-GraphQL-dotnet/Program.cs
+++ Common-GraphQL-dotnet/Common-GraphQL-dotnet/Program.cs
@@ -41,6 +41,7 @@ builder
    })
    .AddMutationType<GamesMutation>() // Add GraphQL root mutation type
    .AddQueryType<GamesQuery>() // Add GraphQL root query type
+   .AddTypeExtension<ReviewerQuery>()
    .ModifyRequestOptions(options =>
    {
        // allow exceptions to be included in response when in development
---
+++ Common-GraphQL-dotnet/Common-GraphQL-dotnet/Query/ReviewerQuery.cs
@@ -0,0 +1,28 @@
+﻿using Common_GraphQL_dotnet.Data.Context;
+using Common_GraphQL_dotnet.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Common_GraphQL_dotnet.Query
+{
+    [ExtendObjectType(OperationTypeNames.Query)]
+    [GraphQLDescription("Queries to manage and query data")]
+    public class ReviewerQuery
+    {
+        [GraphQLDescription("Get a list of reviewers")]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<ReviewerDto> GetReviewers([Service] AppDbContext context) => context
+           .Reviewers
+           .AsNoTracking()
+           .TagWith($"{nameof(ReviewerQuery)}::{nameof(GetReviewers)}")
+           .Select(reviewer => new ReviewerDto
+           {
+               Email = reviewer.Email,
+               ReviewerId = reviewer.Id.ToString(),
+               Name = reviewer.Name,
+               PictureUrl = reviewer.Picture,
+               Username = reviewer.Username
+           });
+    }
+}
