e3af277cb732e4be1f5ceb9c605e57e4c54d3552
common khadka <commonkhadka@gmail.com>
2025-03-19 21:38:22 +0545

    Add default project for rabbit mq chat app

---
+++ Common-RabbitMQ-ChatAPP-dotnet/Common-RabbitMQ-ChatAPP-dotnet/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Common_RabbitMQ_ChatAPP_dotnet.Models;
+
+namespace Common_RabbitMQ_ChatAPP_dotnet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
